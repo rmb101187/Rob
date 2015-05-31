666ece64dd9f3a808db0f748dacd5ca094a382d4
rmb101187 <rmb101187@gmail.com>
2015-05-30 22:00:51 -0700

    05.30.15
    Created the array list for the equipped items, will just need to modify
    values and pass into combat class for character.

--- CharStatEnhancer.java
+++ CharStatEnhancer.java
@@ -1,7 +1,7 @@
 package com.company;
 
 import java.util.*;
-import java.util.concurrent.ConcurrentHashMap;
+
 
 /**
  * Created by Robert on 5/21/2015.
@@ -18,14 +18,15 @@ public class CharStatEnhancer {
     List<Item> mCharacterItems;
     List<Item> mEquippableItems;
     Map<String, String> mCategoryMap;
+    Map<String, Item> mEquippedItems;
+    List<Item> currentlyEquipped;
     private boolean headEquipped;
     private boolean weaponEquipped;
     private boolean torsoEquipped;
     private boolean feetEquipped;
-    private Set headSet;
-    private Set weaponSet;
-    private Set torsoSet;
-    private Set feetSet;
+    Item item;
+
+
 
 
 
@@ -55,22 +56,26 @@ public class CharStatEnhancer {
             if (itemCategory.equalsIgnoreCase("head") && item.getCategory().equalsIgnoreCase("head")) {
                 mCategoryMap.put(item.getCategory(), item.getName());
                 System.out.printf("%s - ", item.getName());
+
             }
             else if (itemCategory.equalsIgnoreCase("weapon") && item.getCategory().equalsIgnoreCase("weapon")) {
                 mCategoryMap.put(item.getCategory(), item.getName());
                 System.out.printf("%s - ", item.getName());
+
             }
             else if (itemCategory.equalsIgnoreCase("torso") && item.getCategory().equalsIgnoreCase("torso")) {
                 mCategoryMap.put(item.getCategory(), item.getName());
                 System.out.printf("%s - ", item.getName());
+
             }
             else if (itemCategory.equalsIgnoreCase("feet") && item.getCategory().equalsIgnoreCase("feet")) {
                 mCategoryMap.put(item.getCategory(), item.getName());
                 System.out.printf("%s - ", item.getName());
+
             }
 
         }
-
+        System.out.println("\n");
         return mCategoryMap;
     }
 
@@ -79,20 +84,26 @@ public class CharStatEnhancer {
         mCharacterItems = Item.createItemList();
         mCategoryMap = new TreeMap<>();
         mEquippableItems = createEquippableList();
+        mEquippedItems = new HashMap<>();
+        currentlyEquipped = new ArrayList<>();
         createEquippableList();
-        System.out.println("What equipment slot would you like to view ");
+        System.out.println("What equipment slot would you like to view \n");
         String equipmentSlot = CharBuilder.sc.nextLine();
         if (equipmentSlot.equalsIgnoreCase("head")) {
             showList("head");
+            equipItem();
         }
         else if (equipmentSlot.equalsIgnoreCase("weapon")) {
             showList("weapon");
+            equipItem();
         }
         else if (equipmentSlot.equalsIgnoreCase("torso")) {
             showList("torso");
+            equipItem();
         }
         else if (equipmentSlot.equalsIgnoreCase("feet")) {
             showList("feet");
+            equipItem();
         }
 
     }
@@ -101,6 +112,37 @@ public class CharStatEnhancer {
         System.out.printf("Your %s equippable items are as follows \n", itemType);
         setCategoryMap(itemType);
     }
+
+    public Map<String, Item> equipItem() {
+        for (Item item : mEquippableItems) {
+            mEquippedItems.put(item.getName(), item);
+        }
+        System.out.println("Would you like to equip one of these items? \n");
+        String equipAnswer = CharBuilder.sc.nextLine();
+        if (equipAnswer.equalsIgnoreCase("yes")) {
+            System.out.println("Which item would you like to equip");
+            String equipChoice = CharBuilder.sc.nextLine();
+            String equipChoiceLower = equipChoice.toLowerCase();
+            item = mEquippedItems.get(equipChoiceLower);
+            mEquippedItems.put(item.getName(), item);
+            currentlyEquipped.add(item);
+
+        }
+        else {
+            System.out.println("Sorry, please choose yes or no");
+            equipItem();
+        }
+
+
+        for (Item item : currentlyEquipped) {
+            System.out.println("You currently have the following items equipped");
+            System.out.printf("%s -", item.getName());
+
+        }
+
+        return mEquippedItems;
+    }
+
 
 
     public int getStr() {

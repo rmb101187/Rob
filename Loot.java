d5710681fdda76aba271fc1276cd2fae9a183358
rmb101187 <rmb101187@gmail.com>
2015-06-07 16:12:26 -0700

    06.07.2015
    Fixed glitch where item was not being added.

--- Loot.java
+++ Loot.java
@@ -50,7 +50,7 @@ public class Loot {
         System.out.printf("Congratulations you found a %s \n", mFoundItem.getName());
         if (rareItemChance > 65) {
             System.out.printf("What luck!! You've found a rare item %s \n", mRareItem.getName());
-            possessedItems.add(mRareItem); // if the enemy is from the hard class drop a rare item
+            CharacterInventory.addPossessedItems(mRareItem); // if the enemy is from the hard class drop a rare item
         }
 
 

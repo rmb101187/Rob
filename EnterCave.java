9302f73af695dec0ec0cdd9fbb41918cfc768ec8
rmb101187 <rmb101187@gmail.com>
2015-06-13 09:34:43 -0700

    06.13.15
    Worked on story, also added option to leave cave and go back to town,
    cleaned up the class more.

--- EnterCave.java
+++ EnterCave.java
@@ -7,48 +7,82 @@ public class EnterCave {
 
     boolean choice1leftExplored;
     boolean choice1rightExplored;
+    boolean choice1center;
     Combat combat = new Combat();
 
     public void enterCave() {
-        System.out.println("You enter teh cave, there appears to be just enough to light to see that that there is a " +
-                "path that leads to the left, and one to the right which way do you take");
+        System.out.printf("You enter the cave, there appears to be just enough to light to see that that there is a " +
+                "path that leads to the left, and one to the right \nthere also appears to be a " +
+                "large room down a center path, but this blocked by a large door. which way do you take? \n" +
+                "You may also leave the cave and head back to town to shop or equip items");
         String firstChoice = CharBuilder.sc.nextLine();
 
         if (firstChoice.equalsIgnoreCase("left")) {
-            if (choice1leftExplored) {
-                System.out.println("You have already explored this area, please choose another room");
-                enterCave();
-            }
             firstRoomleft();
 
         }
         else if (firstChoice.equalsIgnoreCase("right")) {
-            if (choice1rightExplored) {
-                System.out.println("You have already explored this room");
-                enterCave();
-            }
             firstRoomright();
+        }
+        else if (firstChoice.equalsIgnoreCase("center")) {
+            firstRoomCenter();
+        }
+        else if (firstChoice.equalsIgnoreCase("exit") || firstChoice.equalsIgnoreCase("leave")
+         || firstChoice.equalsIgnoreCase("town")) {
+            Chapter1.playChapter1();
         }
 
         else {
-            System.out.println("Sorry, please choose left or right");
+            System.out.println("Sorry, please choose left, right center or exit");
         }
     }
 
     public void firstRoomleft() {
+        if (choice1leftExplored) {
+            System.out.println("You have already explored the left room, please choose another room");
+            enterCave();
+        }
 
         System.out.println("Oh no you run into some monters!");
         combat.combat();
         choice1leftExplored = true;
+        headtoMainRoom();
+        enterCave();
 
     }
 
     public void firstRoomright() {
+        if (choice1rightExplored) {
+            System.out.println("You have already explored the  right room");
+            enterCave();
+        }
         System.out.println("You found an empty room with a skeleton with a arrow stick in it's check YIKES! \n");
         System.out.println("There also appears to be a key clutched in his hand, you rip it off, but a combat ensues!");
         combat.combat();
         choice1rightExplored = true;
+        headtoMainRoom();
+        enterCave();
 
+    }
+
+    public void firstRoomCenter() {
+        if (choice1center) {
+            System.out.println("You have already explored this center room");
+            enterCave();
+        }
+        else if (!choice1rightExplored  || !choice1leftExplored) {
+            System.out.println("There is a large door with 2 keyholes, you cannot enter any further");
+            enterCave();
+        }
+        System.out.println("You arrive at the door, and gently enter both keys, the room unlocks and you move forward");
+        System.out.println("You are quickly attacked by monsters!");
+        combat.combat();
+        System.out.println("This room is very large, you stare in wonder at it");
+        enterCave();
+    }
+
+    public void headtoMainRoom() {
+        System.out.println("Having explored this room, you head back to the main cave");
     }
 
 
